a90f91b93fe598958b8803483bd877962d6453d6
Nghi Tran <nghi.t.tran8@gmail.com>
2020-03-05 09:33:28 -0800

    init commit

---
+++ linkedList.js
@@ -0,0 +1,19 @@
+// #1 Create a linked list class
+
+// #2 Create a singly linked list
+
+// #3 Supplemental functions for a linked list
+
+// #4 Mystery program
+
+// #5 Reverse a list
+
+// #6 3rd from the end
+
+// #7 Middle of a list
+
+// #8 Cycle in a list
+
+// #9 Doubly linked list
+
+// #10 Reverse a DLL
