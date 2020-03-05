b3012af3cfe9aa71cf692078bfeef3c242717587
Nghi Tran <nghi.t.tran8@gmail.com>
2020-03-05 11:36:19 -0800

    insert First

--- ourLinkedList.js
+++ ourLinkedList.js
@@ -74,9 +74,48 @@ class LinkedList {
     }
     previousNode.next = currNode.next;
   }
+
+  insertBefore(item, itemBefore) {
+    // Check if list is empty
+    if (this.head === null) {
+      this.insertFirst(item);
+      console.log("List was empty - added as first item");
+      return;
+    }
+
+    let previous = this.head;
+    let current = this.head;
+    // Check to make sure if item is what im looking for and whether it's last
+    while (current.next !== null && current.value !== itemBefore) {
+      previous = current;
+      current = current.next;
+    }
+    // Check if item found and is the first item on list
+    if (this.head === current && current.value === itemBefore) {
+      this.head = new _Node(item, current);
+    }
+    // Check if item found and is not the first item on list
+    else if (current.value === itemBefore) {
+      return (previous.next = new _Node(item, current));
+    }
+    // All else
+    else {
+      console.log("Item not found");
+      return;
+    }
+  }
 }
 
 function main() {
   SLL = new LinkedList();
   SLL.insertFirst("Apollo");
+  SLL.insertFirst("Boomer");
+  SLL.insertFirst("Helo");
+  SLL.insertFirst("Husker");
+  SLL.insertFirst("Starbuck");
+  SLL.insertFirst("Tauhida");
+  SLL.remove("Squirrel");
+  SLL.insertBefore("Now", "Boomer");
 }
+
+main();
